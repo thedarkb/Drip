95d5b3fdb4d91e4e9cb5d21dc45a0f517cdc6e40
thedarkb <thedarkb@github.com>
2020-02-16 14:47:05 +0000

    Added faction based clothing colours.

---
+++ clothes.c
@@ -0,0 +1,18 @@
+outfit cloth_default() {
+	outfit me;
+	memset(&me,0,sizeof me);
+	me.colourFrame[0]=209;
+	me.colourFrame[1]=185;
+	me.colourFrame[2]=193;
+	me.colourFrame[3]=193;
+	me.colourFrame[4]=208;
+	me.colourFrame[5]=184;
+	me.colourFrame[6]=192;
+	me.colourFrame[7]=200;
+	me.colourFrame[8]=208;
+	me.colourFrame[9]=210;
+	me.colourFrame[10]=186;
+	me.colourFrame[11]=194;
+	me.colourFrame[12]=202;
+	return me;
+}
