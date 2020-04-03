ea1c0f137f97cef1bebf2fa6805a0a80b39c6aff
thedarkb <thedarkb@github.com>
2020-04-03 15:35:30 +0100

    Forgot the map editor when >_<

---
+++ mapeditor.c
@@ -0,0 +1,90 @@
+view* editorArray[705][705];
+unsigned int mapEditorTile=0;
+
+void initMapEditor() {memset(&editorArray,0,sizeof editorArray);}
+
+void mapEditorShim(view* in, int x, int y){
+	if(!editorArray[x][y]) {
+		editorArray[x][y]=malloc(sizeof tilewrapper[1][1]);
+	}
+	*editorArray[x][y]=*in;
+}
+
+void editorMapExport() {
+	if(!editorArray[sX][sY]) return;
+	FILE* fileOut=NULL;
+
+	printf("Output Filename: ");
+	char filename[64];
+	char structName[64];
+	memset(&filename,0,sizeof filename);
+	scanf("%s", structName);
+	sprintf(filename,"%s.c",structName);
+	fileOut=fopen(filename, "w");
+
+	fprintf(fileOut, "view %s={\n", structName);
+	fprintf(fileOut,"	{\n");
+	for(int y=0;y<SH;y++){
+		fprintf(fileOut,"		{");
+		for(int x=0;x<SW;x++){
+			if(x!=SW-1)fprintf(fileOut,"%u,",(*editorArray[sX][sY]).screen[y][x]);
+			else fprintf(fileOut,"%u",(*editorArray[sX][sY]).screen[y][x]);
+		}
+		if(y!=SH-1) fprintf(fileOut,"},\n");
+		else fprintf(fileOut,"}\n");
+	}
+	fprintf(fileOut,"	},\n	{\n");
+	for(int y=0;y<SH;y++){
+		fprintf(fileOut,"		{");
+		for(int x=0;x<SW;x++){
+			if(x!=SW-1)fprintf(fileOut,"%u,",(*editorArray[sX][sY]).layers[y][x]);
+			else fprintf(fileOut,"%u",(*editorArray[sX][sY]).layers[y][x]);
+		}
+		if(y!=SH-1) fprintf(fileOut,"},\n");
+		else fprintf(fileOut,"}\n");
+	}
+	fprintf(fileOut,"	}\n");	
+	fprintf(fileOut,"}");
+	fclose(fileOut);
+}
+
+void drawEditorOverlay(){
+	static int lastTap=0;
+	if(!lastTap && keyboard[SDL_SCANCODE_PAGEDOWN] && mapEditorTile<TILECOUNT) mapEditorTile++;
+	else if (!lastTap && keyboard[SDL_SCANCODE_PAGEUP] && mapEditorTile>0) mapEditorTile--;
+	else if(!lastTap&&keyboard[SDL_SCANCODE_T]) {
+		printf("Enter new sX: ");
+		scanf("%u", &sX);
+		printf("Enter new sY: ");
+		scanf("%u", &sY);
+		refresh=1;
+	}
+
+	if(!lastTap && keyboard[SDL_SCANCODE_M]) editorMapExport();
+
+	if(keyboard[SDL_SCANCODE_PAGEUP] || keyboard[SDL_SCANCODE_PAGEDOWN] || keyboard[SDL_SCANCODE_M]) lastTap=1;
+	else lastTap=0;
+
+	if(keyboard[SDL_SCANCODE_RIGHTBRACKET]&&mapEditorTile<TILECOUNT-1)mapEditorTile+=2;
+	if(keyboard[SDL_SCANCODE_LEFTBRACKET]&&mapEditorTile>1)mapEditorTile-=2;
+
+	for(int x=0;x<SW;x++) {
+		for(int y=0;y<SH;y++) {
+			if(!keyboard[SDL_SCANCODE_B]){
+				if(tilewrapper[1][1].layers[y][x]) drawRectAlphaTrack(x*TS, y*TS, TS, TS, 0, 32);
+				else drawRectAlphaTrack(x*TS, y*TS, TS, TS, 0xFFFFFF, 32);
+			} else {
+				if(tilewrapper[1][1].layers[y][x]) drawRectAlphaTrack(x*TS, y*TS, TS, TS, 0, 255);
+				else drawRectAlphaTrack(x*TS, y*TS, TS, TS, 0xFFFFFF, 255);
+			}
+		}
+	}
+
+	drawRect(0,0,SW*TS,TS*2,0);
+
+	char editorText[255];
+	sprintf(editorText,"Tile ID: %u\nGrid Position: %u,%u\n",mapEditorTile,sX,sY);
+	text(editorText,TS+2,-TS);
+
+	hudDraw(hwtileset[mapEditorTile],0,0,TS,TS);
+}
